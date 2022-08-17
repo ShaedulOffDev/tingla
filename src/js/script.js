c3b84a2979809f94848429926208c7d9122048db
Shaedul_Off <95744806+ShaedulOffDev@users.noreply.github.com>
2022-08-17 11:39:32 +0500

    last

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 let $ = e => document.querySelector(e);
 window.addEventListener('load', (e)=>{
     $('.loader').classList.add('hide')
+    setTimeout(e => {
+        $('.loader').style.display = 'none'
+    }, 300)
 })
